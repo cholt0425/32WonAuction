4f16eef092e08fc502459fd95b7fb448dec977b9
cholt0425 <cholt@convergencedata.com>
2021-10-14 07:59:58 -0400

    Changes that added Web application and got all the applications building in unison and being targeted to linux docker containers.

---
+++ Auction/Settings/MongoDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auction.Settings
+{
+    public class MongoDbConfig
+    {
+        public string Name { get; init; }
+        public string Host { get; init; }
+        public int Port { get; init; }
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+    }
+}
